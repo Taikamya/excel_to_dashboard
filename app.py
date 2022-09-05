04010b8bfcc2d26afde8969f9512639fc73e4b76
Dimitri Dias Moreira <taikamya@outlook.com>
2022-09-05 15:30:41 +0200

    type ignore

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from local_settings import EXCEL
 
 import pandas as pd
-# import plotly.express as px
-# import streamlit as st
+import plotly.express as px
+import streamlit as st
+
+st.set_page_config(page_title="Sample Dashboard",
+                   page_icon=":bar_chart:")
+
 
 with open(EXCEL, "rb") as f:
     data = f.read()
@@ -10,11 +14,11 @@ with open(EXCEL, "rb") as f:
 df = pd.read_excel(
     io=data,
     engine='openpyxl',
-    sheet_name='Sales',
+    sheet_name='Sample',
     skiprows=3,
     usecols='B:R',
     nrows=1100,
-)
+)   # type:ignore
 df.index += 1
 
 dframe = pd.DataFrame(df)
